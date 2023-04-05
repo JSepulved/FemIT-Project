83c0a9a5828ad924c25a33797aa975b306cef7db
Seigi Gim <80078548+SeigiGim@users.noreply.github.com>
2023-04-05 11:21:21 -0400

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Banner from "./Banner/Banner";
+import Button from "./Button/Button";
+import Card from "./Card/Card";
+import Comment from "./Comment/Comment";
+import Footer from "./Footer/Footer";
+import FormRegister from "./Form/FormRegister";
+import FormLogIn from "./Form/FormLogIn";
+import Image from "./Image/Image";
+import Logo from "./Logo/Logo";
+import Modal from "./Modal/Modal";
+import Navbar from "./Navbar/Navbar";
+import NavbarLanding from "./Navbar/NavbarLanding";
+import Portfolio from "./Portfolio/Portfolio";
+import Publication from "./Publication/Publication";
+import Reactions from "./Reactions/Reaction";
+
+export {
+	Banner,
+	Button,
+	Card,
+	Comment,
+	Footer,
+	FormRegister,
+	FormLogIn,
+	Image,
+	Logo,
+	Modal,
+	Navbar,
+	NavbarLanding,
+	Portfolio,
+	Publication,
+	Reactions,
+};
